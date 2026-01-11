700283a46c38334792ce744f12669a6622bee67a
Mayank Gaur <mayankgaur13004@gmail.com>
2026-01-11 10:57:41 +0530

    Solved1900A

---
+++ cf1900A.py
@@ -0,0 +1,21 @@
+t=int(input())
+
+def fun(n,A):
+        total = 0
+        temp_c = 0
+        for i in A:
+                if i == '#':
+                    temp_c = 0
+                
+                    total += 1
+                    temp_c += 1
+                    if temp_c > 2:
+                        return 2
+        return total
+        
+
+for i in range(t):
+        n = int(input())
+        L = input()
+        print(fun(n,L))
+        
