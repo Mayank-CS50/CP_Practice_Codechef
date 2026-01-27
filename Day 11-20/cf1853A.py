9da21cf42b4d5246858dc9388ac11c6ce13f7dae
Mayank Gaur <mayankgaur13004@gmail.com>
2026-01-28 02:49:37 +0530

    solved 1853A

--- Day 11-20/cf1853A.py
+++ Day 11-20/cf1853A.py
@@ -10,13 +10,10 @@ def fun(n,A):
         for i in range(n-1):
             if abs(A[i]-A[i+1])<chota_diff:
                 chota_diff=abs(A[i]-A[i+1])
-            print(chota_diff,'jj')
+            # print(chota_diff,'jj')
         temp=chota_diff/2
-        if temp==1:
-            return 1
-        else:
-            return (chota_diff//2)+1
-     
+        return int(temp)+1
+
 for _ in range(int(input())):
     n=int(input())
     A=list(map(int,input().split()))
